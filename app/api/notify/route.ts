05f064bd5fd5f729a545d4bd809f040ad183ef2a
MattJRead <matthewjread13@gmail.com>
2026-06-08 02:51:37 +0100

    Launch Day MVP

--- app/api/notify/route.ts
+++ app/api/notify/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-//const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.re_fQ4HHUwp_3gdBTZe91JVmEn4rF4Nu6HxZ);
 
 export async function POST(request: Request) {
   try {
