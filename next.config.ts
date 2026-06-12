994e43054ec88e1eca7a283682db6842b36c2a55
MattJRead <matthewjread13@gmail.com>
2026-06-12 11:51:01 +0100

    Unlock Next.js image domains for TypeScript

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'books.google.com' },
-      { protocol: 'https', hostname: 'covers.openlibrary.org' },
-      { protocol: 'https', hostname: 'jackets.gardners.com' },
-      { protocol: 'https', hostname: 'jackets.dmmserver.com' },
-      { protocol: 'https', hostname: 'www.waterstones.com' },
-      { protocol: 'https', hostname: 'waterstones.com' },
-      { protocol: 'https', hostname: 'blackwells.co.uk' },
-      { protocol: 'https', hostname: 'www.blackwells.co.uk' }
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
     ],
   },
 };
