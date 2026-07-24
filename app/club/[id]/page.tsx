29f34ef6c1f3d7a1cfe2a271d140d72e3eca49f3
MattJRead <matthewjread13@gmail.com>
2026-07-24 02:57:48 +0100

    disabled static caching for a page

--- app/club/[id]/page.tsx
+++ app/club/[id]/page.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+export const dynamic = 'force-dynamic';
+
+import { useState, useEffect } from 'react';
 import { supabase } from '../../../lib/supabase';
 import UpdateProgress from './UpdateProgress';
 import ActivePoll from './ActivePoll';
